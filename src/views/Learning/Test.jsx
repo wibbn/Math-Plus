5fc3999e70ebe2140b3203b1aa1568db0c3397c4
Ivan Lazunin <ivlazunin@ya.ru>
2019-04-06 13:00:29 +0300

    Connect to a database with tests

--- src/views/Learning/Test.jsx
+++ src/views/Learning/Test.jsx
@@ -13,8 +13,6 @@ import CustomInput from 'components/CustomInput/CustomInput.jsx';
 
 import testPageStyle from 'assets/jss/material-kit-react/views/testPage.jsx';
 import {compose} from "redux";
-import {connect} from "react-redux";
-import {firestoreConnect} from "react-redux-firebase";
 import firebase from 'firebase/app';
 
 const dashboardRoutes = [];
@@ -27,28 +25,29 @@ class Test extends React.Component {
         };
     }
 
-    renderTasks = async() => {
-        const id = Number(this.props.match.params.id);
-        let ts = [];
-        firebase.firestore().collection('tasks').where('test_num', '==', id).orderBy('id').get().then(function(querySnapshot) {
-                querySnapshot.forEach(function (doc) {
-                    // console.log(doc.data().task);
-                    ts.push(doc.data().task);
-                })
-        });
+    getTasks = async(test_num = 1) => {
+        const db = firebase.firestore();
+        const collection = db.collection('tasks');
+        const query = collection
+            .where('test_num', '==', test_num)
+            .orderBy('id');
+        const snapshot = await query.get();
+        return snapshot.docs.map(doc => (doc.data()));
+    };
 
-        this.setState({tasks: ts})
+    runQuery = async(test_num) => {
+        const data = await this.getTasks(test_num);
+        this.setState({tasks: data});
     };
 
     componentDidMount() {
-        this.renderTasks();
+        const id = Number(this.props.match.params.id);
+        this.runQuery(id);
     }
 
     render() {
         const { classes, ...rest } = this.props;
         const id = Number(this.props.match.params.id);
-        const tasks = this.state.tasks;
-        // console.log(id);
         return (
             <div>
                 <Header
@@ -80,22 +79,16 @@ class Test extends React.Component {
                                             Вариант схожий с настоящим экзаменационным тестом. В поля ответов вписывать то, что требуется в задаче. При наличие ответов вписывать через ';'. На часть С принимаются только ответы (без решений) На решение теста отводится 3 часа 55 минут. Успепхов!
                                         </h5>
                                     </GridItem>
-                                    {console.log('this.state.tasks = ', this.state.tasks)}
-                                    {console.log('this.state.tasks[0] = ', this.state.tasks[0])}
-                                    {this.state.tasks.forEach(function (x) {
-                                        console.log(x);
-                                    })}
-                                    {/*{this.state.tasks.map(x => console.log(x))}*/}
-                                    {/*this.state.tasks && */this.state.tasks.map(  x =>
+                                    {this.state.tasks && this.state.tasks.map(x =>
                                         <GridItem xs={12} sm={12} md={12}>
                                             <div className={classes.task}>
-                                                <h3 className={classes.taskNum}>Задание {x.id+1} <h5 className={classes.taskName}>{x.name}</h5></h3>
+                                                <h3 className={classes.taskNum}>Задание {x.id} <h5
+                                                    className={classes.taskName}>{x.name}</h5></h3>
                                                 <h5 className={classes.description}>{x.task}</h5>
                                                 <div className={classes.inputForm}>
                                                     <CustomInput
-
                                                         labelText='Ответ...'
-                                                        id={'ans'+x.id}
+                                                        id={'ans' + x.id}
                                                         formControlProps={{
                                                             fullWidth: true
                                                         }}
@@ -107,6 +100,7 @@ class Test extends React.Component {
                                             </div>
                                         </GridItem>
                                     )}
+
                                 </GridContainer>
                             </div>
                         </div>
@@ -118,24 +112,8 @@ class Test extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        themes: state.firestore.ordered.themes,
-        tasks: state.firestore.ordered.themes
-    }
-};
+
 
 export default compose(
     withStyles(testPageStyle)
-    // connect(mapStateToProps),
-    // firestoreConnect([
-    //     {
-    //         collection: 'themes',
-    //         orderBy: 'id'
-    //     }, {
-    //         collection: 'tasks',
-    //         where: ['test_num', '==',  this.props.match.params.id],
-    //         orderBy: 'id'
-    //     }
-    //     ])
 )(Test)
